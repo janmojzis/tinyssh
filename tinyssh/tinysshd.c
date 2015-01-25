5d3a2e658135d7ba9f4fbe9ed8e98a929ab5c76a
apache <apache@apache.kobka>
2015-01-25 16:00:57 +0100

    typo

--- tinyssh/tinysshd.c
+++ tinyssh/tinysshd.c
@@ -45,7 +45,7 @@ Public domain.
    -S (optional): disable state-of-the-art crypto\n\
    -p (optional): enable post-quantum crypto - TODO\n\
    -P (optional): disable post-quantum crypto\n\
-   -l (optional): use syslog instead of standard error output (usefull for running from inetd)\n\
+   -l (optional): use syslog instead of standard error output (useful for running from inetd)\n\
    -L (optional): don't use syslog, use standard error output (default)\n\
    -x name=command (optional): add subsystem command (example: sftp=/usr/libexec/openssh/sftp-server)\n\
    keydir: directory containing secret and public SSH keys for signing\n\
