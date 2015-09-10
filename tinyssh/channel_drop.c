883a05a6991b571b2af6b86a16e2b1f29e7b9db5
apache <jan.mojzis@firma.seznam.cz>
2015-09-10 10:54:36 +0200

    tty permissions fix

--- tinyssh/channel_drop.c
+++ tinyssh/channel_drop.c
@@ -4,42 +4,12 @@ Jan Mojzis
 Public domain.
 */
 
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <pwd.h>
-#include <grp.h>
 #include "dropuidgid.h"
 #include "newenv.h"
 #include "channel.h"
-
-static int setowner(char *name, struct passwd *pw) {
-
-    struct group *grp;
-    gid_t gid;
-    mode_t mode;
-    struct stat st;
-
-    grp = getgrnam("tty");
-    if (grp) {
-        gid = grp->gr_gid;
-        mode = S_IRUSR | S_IWUSR | S_IWGRP;
-    }
-    else {
-        gid = pw->pw_gid;
-        mode = S_IRUSR | S_IWUSR | S_IWGRP | S_IWOTH;
-    }
-
-    if (stat(name, &st) == -1) return 0;
-    if (chown(name, pw->pw_uid, gid) == -1) return 0;
-
-    if ((st.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO)) != mode) {
-        if (chmod(name, mode) == -1) return 0;
-    }
-
-    return 1;
-}
-
 
 int channel_droppriv(char *user, char **shell) {
 
@@ -53,7 +23,9 @@ int channel_droppriv(char *user, char **shell) {
         name = ttyname(0);
         if (!name) return 0;
         if (!newenv_env("SSH_TTY", name)) return 0;
-        if (!setowner(name, pw)) return 0;
+        /* setowner */
+        if (chown(name, pw->pw_uid, pw->pw_gid) == -1) return 0;
+        if (chmod(name, 0600) == -1) return 0;
     }
 
     /* drop privileges */
