41ab27aa00a81de63f4e6ad554f5e5cd23e3c436
Jan Mojzis <github@mojzis.com>
2016-01-30 10:40:54 +0100

    added log file

--- make-tinyssh.sh
+++ make-tinyssh.sh
@@ -3,25 +3,48 @@
 top="`pwd`"
 build="`pwd`/build"
 bin="${build}/bin"
-lib="$build/lib"
-include="$build/include"
-work="$build/work"
+lib="${build}/lib"
+include="${build}/include"
+log="${build}/log"
+work="${build}/work"
 contribdir="`pwd`/build-contrib"
 
-rm -rf "$build"
-mkdir -p "$build"
-mkdir -p "$bin"
-mkdir -p "$lib"
-mkdir -p "$include"
+rm -rf "${build}"
+mkdir -p "${build}"
+mkdir -p "${bin}"
+mkdir -p "${lib}"
+mkdir -p "${include}"
+
+exec 5>"${log}"
+exec 2>&5
+exec </dev/null
+
+log0() {
+  echo "=== `date` === $@" >&5
+}
+
+log1() {
+  echo "=== `date` === $@"
+  echo "=== `date` === $@" >&5
+}
+
+log2() {
+  echo "=== `date` ===   $@"
+  echo "=== `date` ===   $@" >&5
+}
 
 version=`head -1 "${top}/debian/changelog"  | cut -d '(' -f2 | cut -d ')' -f1` #XXX
 
 LANG=C
 export LANG
 
-echo "=== `date` === obtaining compiler"
-rm -rf "$work"
-mkdir -p "$work"
+log0 "uname -a: `uname -a || :`"
+log0 "uname -F: `uname -F || :`"
+log0 "uname -M: `uname -M || :`"
+
+log1 "obtaining compiler"
+rm -rf "${work}"
+mkdir -p "${work}"
 (
   cd "${work}"
   (
@@ -32,127 +55,123 @@ mkdir -p "$work"
   ) | while read compiler
   do
     echo 'int main(void) { return 0; }' > try.c
-    ${compiler} -o try try.c 2>/dev/null || { echo "=== `date` ===   ${compiler} failed"; continue; }
-    echo "=== `date` ===   ${compiler} ok"
+    ${compiler} -o try try.c || { log2 "${compiler} failed"; continue; }
+    log2 "${compiler} ok"
     echo "${compiler}" > compiler
     break
   done
 )
 compiler=`head -1 "${work}/compiler"`
-echo "=== `date` === finishing"
+log1 "finishing"
 
-echo "=== `date` === checking compiler options"
-rm -rf "$work"
-mkdir -p "$work"
+log1 "checking compiler options"
+rm -rf "${work}"
+mkdir -p "${work}"
 (
   cd "${work}"
-  cflags=`cat "${top}/conf-cflags" 2>/dev/null || :`
+  cflags=`cat "${top}/conf-cflags" || :`
   cflags="${CFLAGS} ${LDFLAGS} ${cflags}"
 
   for i in ${cflags}; do
     echo 'int main(void) { return 0; }' > try.c
-    ${compiler} "$i" -o try try.c 2>/dev/null || { echo "=== `date` ===   $i failed"; continue; }
-    options="$i $options"
-    echo "=== `date` ===   $i ok"
+    ${compiler} "${i}" -o try try.c || { log2 "${i} failed"; continue; }
+    options="${i} ${options}"
+    log2 "${i} ok"
   done
-  echo $options > options
+  echo ${options} > options
 )
 compilerorig=${compiler}
 compiler="${compiler} `cat ${work}/options`"
-echo "=== `date` ===   $compiler"
-echo "=== `date` === finishing"
+log2 "${compiler}"
+log1 "finishing"
 
-echo "=== `date` === checking libs"
-rm -rf "$work"
-mkdir -p "$work"
+log1 "checking libs"
+rm -rf "${work}"
+mkdir -p "${work}"
 (
-  cd "$work"
+  cd "${work}"
   (
-    exec 2>/dev/null
     cat "${top}/conf-libs" || :
   ) | (
-    exec 5>syslibs
+    exec 9>syslibs
     while read i; do
       echo 'int main(void) { return 0; }' > try.c
-      ${compiler} ${i} -o try try.c 2>/dev/null || { echo "=== `date` ===   ${i} failed"; continue; }
-      echo "${i}" >&5
-      echo "=== `date` ===   $i ok"
+      ${compiler} ${i} -o try try.c || { log2 "${i} failed"; continue; }
+      echo "${i}" >&9
+      log2 "${i} ok"
     done
   )
 )
 libs=`cat "${work}/syslibs"`
-echo "=== `date` === finishing"
+log1 "finishing"
 
-echo "=== `date` === checking \$LIBS"
+log1 "checking \$LIBS"
 if [ x"${LIBS}" != x ]; then
-  rm -rf "$work"
-  mkdir -p "$work"
+  rm -rf "${work}"
+  mkdir -p "${work}"
   (
-    cd "$work"
+    cd "${work}"
     for i in ${LIBS}; do
       echo 'int main(void) { return 0; }' > try.c
-      ${compiler} $i -o try try.c 2>/dev/null || { echo "=== `date` ===   $i failed"; continue; }
-      syslibs="$i $syslibs"
-      echo "=== `date` ===   $i ok"
+      ${compiler} ${i} -o try try.c || { log2 "${i} failed"; continue; }
+      syslibs="${i} ${syslibs}"
+      log2 "${i} ok"
     done
-    echo $syslibs > syslibs
+    echo ${syslibs} > syslibs
  )
 fi
 libsorig=${libs}
 libs="${libs} `cat ${work}/syslibs`"
-echo "=== `date` === finishing"
+log1 "finishing"
 
-
-echo "=== `date` === building sysdep headers"
-rm -rf "$work"
-mkdir -p "$work"
-cp -pr sysdep/* "$work"
+log1 "building sysdep headers"
+rm -rf "${work}"
+mkdir -p "${work}"
+cp -pr sysdep/* "${work}"
 (
-  cd "$work"
+  cd "${work}"
   sh list | (
     while read target source
     do
       [ -f "${include}/${target}" ] && continue
-      rm -f "$source" "$target.tmp" 
-      ${compiler} -O0 -o "$source" "$source.c" $libs 2>/dev/null || continue
-      ./$source > "$target.tmp" 2>/dev/null || continue
-      cp "$target.tmp" "$include/$target"
-      echo "=== `date` ===   $target $source"
+      rm -f "${source}" "${target}.tmp" 
+      ${compiler} -O0 -o "${source}" "${source}.c" ${libs} || continue
+      ./${source} > "${target}.tmp" || continue
+      cp "${target}.tmp" "${include}/${target}"
+      log2 "${target} ${source}"
     done
   )
 )
-echo "=== `date` === finishing"
+log1 "finishing"
 
-echo "=== `date` === starting crypto lib"
-rm -rf "$work"
-mkdir -p "$work"
-cp -pr crypto/* "$work"
+log1 "starting crypto lib"
+rm -rf "${work}"
+mkdir -p "${work}"
+cp -pr crypto/* "${work}"
 (
-  cd "$work"
+  cd "${work}"
   cat CRYPTOSOURCES\
   | while read x
   do
-    $compiler -I"$include" -c "$x.c" || exit 111
-    #echo "=== `date` ===   $x.o ok"
+    ${compiler} -I"${include}" -c "${x}.c" || { log2 "libtinynacl.a failed ... see the log ${log}"; exit 111; }
   done
   ar cr "${lib}/libtinynacl.a" `cat CRYPTOLIBS` || exit 0
-  #cp *.h "${include}"
 )
-echo "=== `date` ===   libtinynacl.a ok"
-echo "=== `date` === finishing"
+log2 "libtinynacl.a ok"
+log1 "finishing"
 
 origlibs="${origlibs} ${lib}/libtinynacl.a"
 libs="${libs} ${lib}/libtinynacl.a"
 
-echo "=== `date` === starting crypto headers"
-rm -rf "$work"
-mkdir -p "$work"
-cp -p crypto/CRYPTOPRIMITIVES "$work"
-cp -pr crypto-tests/*test.c "$work"
-cp -pr crypto-tests/*.h "$work"
-cp -pr crypto-tests/*.data "$work" 2>/dev/null || :
+log1 "starting crypto headers"
+rm -rf "${work}"
+mkdir -p "${work}"
+cp -p crypto/CRYPTOPRIMITIVES "${work}"
+cp -pr crypto-tests/*test.c "${work}"
+cp -pr crypto-tests/*.h "${work}"
+cp -pr crypto-tests/*.data "${work}" 2>/dev/null || :
 (
-  cd "$work"
+  cd "${work}"
   cat CRYPTOPRIMITIVES\
   | while read primitive checkflag
   do
@@ -178,26 +197,25 @@ cp -pr crypto-tests/*.data "$work" 2>/dev/null || :
       echo 'return 0; }'
     ) > try.c
     #try ext. crypto library
-    if ${compiler} -c "${testf}.c" ${libs} 2>try.log; then
+    if ${compiler} -c "${testf}.c" ${libs}; then
       if ${compiler} -o "${testf}" "${testf}.o" ${libs}; then
-        if ${compiler} -o try try.c 2>try.log; then
-          if /bin/sh -ec "./${testf}" 2>try.log; then
-            echo "=== `date` ===   ${primitive}.h (`./try`) ok"
+        if ${compiler} -o try try.c; then
+          if /bin/sh -ec "./${testf}"; then
+            log2 "${primitive}.h (`./try`) ok"
             echo "#include <${primitive}.h>" >> crypto.h
             continue
           else
-            cat try.log
-            echo "=== `date` ===   ${primitive}.h (`./try`) failed"
+            log2 "${primitive}.h (`./try`) failed"
           fi
         fi
       fi
     fi
     #try int. crypto library
-    if cp -p "${top}/crypto/${primitive}.h" . 2>try.log; then
-      if ${compilerorig} -I. -I"$include" -o "${testf}" "${testf}.c" ${origlibs} 2>try.log; then
-        if ${compilerorig} -I. -I"$include" -o try try.c 2>try.log; then
-          if /bin/sh -ec "./${testf}" 2>try.log; then
-            echo "=== `date` ===   ${primitive}.h (`./try`) ok"
+    if cp -p "${top}/crypto/${primitive}.h" . ; then
+      if ${compilerorig} -I. -I"$include" -o "${testf}" "${testf}.c" ${origlibs}; then
+        if ${compilerorig} -I. -I"$include" -o try try.c; then
+          if /bin/sh -ec "./${testf}"; then
+            log2 "${primitive}.h (`./try`) ok"
             echo "#include \"${primitive}.h\"" >> crypto.h
             cp -p "${primitive}.h" "${include}"
             continue
@@ -205,88 +223,83 @@ cp -pr crypto-tests/*.data "$work" 2>/dev/null || :
         fi
       fi
     fi
-    cat try.log
-    echo "=== `date` ===   ${primitive}.h failed"
+    log2 "${primitive}.h failed ... see the log ${log}"
     exit 111
   done
-  cp crypto.h "$include/crypto.h"
+  cp crypto.h "${include}/crypto.h"
 )
-echo "=== `date` === finishing"
+log1 "finishing"
 
-rm -rf "$work"
-mkdir -p "$work"
-cp -pr tinyssh/* "$work"
-cp -pr tinyssh-tests/*test.c "$work"
-cp -pr tinyssh-tests/*.h "$work"
-cp -pr _tinyssh/* "$work" 2>/dev/null || :
+rm -rf "${work}"
+mkdir -p "${work}"
+cp -pr tinyssh/* "${work}"
+cp -pr tinyssh-tests/*test.c "${work}"
+cp -pr tinyssh-tests/*.h "${work}"
+cp -pr _tinyssh/* "${work}" 2>/dev/null || :
 (
-  cd "$work"
+  cd "${work}"
   touch SOURCES TARGETS _TARGETS
   cat SOURCES TARGETS _TARGETS\
   | while read x
   do
-    $compiler "-DCOMPILER=\"${compilerorig}\"" "-DVERSION=\"${version}\"" -I"$include" -c "$x.c" || exit 111
-    #echo "=== `date` ===   $x.o ok"
+    ${compiler} "-DCOMPILER=\"${compilerorig}\"" "-DVERSION=\"${version}\"" -I"${include}" -c "${x}.c" || exit 111
   done
   ar cr libtinyssh.a `cat LIBS` || exit 111
 
   #tests
-  echo "=== `date` === starting tinyssh-tests"
+  log1 "starting tinyssh-tests"
   cat LIBS \
   | while read x
   do
     t=`echo ${x} | sed 's/.o$/test/'`
     if [ ! -h "${t}.c" ]; then
-      $compiler -I"$include" -c "$t.c" || exit 111
-      $compiler -I"$include" -o "${t}" "${t}.o" libtinyssh.a $libs || exit 111
-      "./$t" || ( echo "=== `date` ===   $t FAILED !!!!!!!!!!!!!!!!!!!!!!!"; exit 111; ) || exit 111
-      echo "=== `date` ===   $t ok"
+      ${compiler} -I"${include}" -c "${t}.c" || { log2 "${t} failed ... see the log ${log}"; exit 111; }
+      ${compiler} -I"${include}" -o "${t}" "${t}.o" libtinyssh.a ${libs} || { log2 "${t} failed ... see the log ${log}"; exit 111; }
+      "./${t}" || { log2 "${t} failed ... see the log ${log}"; exit 111; }
+      log2 "${t} ok"
     fi
   done || exit 111
-  echo "=== `date` === finishing"
+  log1 "finishing"
 
-  #echo "=== `date` ===   libtinyssh.a ok"
-
-  echo "=== `date` === starting _tinyssh"
+  log1 "starting _tinyssh"
   cat _TARGETS \
   | while read x
   do
-    $compiler -I"$include" -o "$x" "$x.o" libtinyssh.a $libs || exit 111
-    echo "=== `date` ===   $x ok"
-    cp -p "$x" "$bin/$x";
+    ${compiler} -I"${include}" -o "${x}" "${x}.o" libtinyssh.a ${libs} || { log2 "${x} failed ... see the log"; exit 111; }
+    log2 "${x} ok"
+    cp -p "${x}" "${bin}/${x}";
   done
-  echo "=== `date` === finishing"
+  log1 "finishing"
 
-  echo "=== `date` === starting tinyssh"
+  log1 "starting tinyssh"
   cat TARGETS \
   | while read x
   do
-    $compiler -I"$include" -o "$x" "$x.o" libtinyssh.a $libs || exit 111
-    echo "=== `date` ===   $x ok"
+    ${compiler} -I"${include}" -o "${x}" "${x}.o" libtinyssh.a ${libs} || { log2 "${x} failed ... see the log"; exit 111; }
+    log2 "${x} ok"
   done
-  echo "=== `date` === finishing"
+  log1 "finishing"
 
-  echo "=== `date` === starting tinyssh regression tests"
+  log1 "starting tinyssh regression tests"
   cat TARGETS \
   | while read x
   do
     sh ${x}.rts > ${x}.out
-    cmp "${x}.out" "${x}.exp" || { echo "=== `date` ===   ${x} TEST FAILED !!!!!!!!!!!!!!!!!!!!!!!"; diff "${x}.out" "${x}.exp"; exit 111; }
-    echo "=== `date` ===   $x ok"
-    cp -p "$x" "$bin/$x";
+    cmp "${x}.out" "${x}.exp" || { log2 "${x} regression test failed ... see the difference `pwd`/${x}.out `pwd`/${x}.exp"; exit 111; }
+    log2 "${x} ok"
+    cp -p "${x}" "${bin}/${x}";
   done || exit 111
-  echo "=== `date` === finishing"
+  log1 "finishing"
 
 ) || exit 111
 
 
-echo "=== `date` === counting words of code - tests"
-rm -rf "$work"
-mkdir -p "$work"
+log1 "counting words of code - tests"
+rm -rf "${work}"
+mkdir -p "${work}"
 
 for dir in tinyssh-tests crypto-tests _tinyssh; do
   (
-    exec 2>/dev/null
     touch "${work}/${dir}"
     [ -d "${dir}" ] || exit 0
     cd "${dir}" 
@@ -297,11 +310,10 @@ for dir in tinyssh-tests crypto-tests _tinyssh; do
     cd "${work}"
     cat "${dir}" \
     | (
-      exec 2>/dev/null;
       cpp -fpreprocessed || gcpp -fpreprocessed
     ) | (
       x=`sed 's/[_a-zA-Z0-9][_a-zA-Z0-9]*/x/g' | tr -d ' \012' | wc -c | tr -d ' '`
-      echo "=== `date` ===   ${dir} $x"
+      log2 "${dir} ${x}"
     )
   )
 done
@@ -309,23 +321,21 @@ done
   cd "${work}"
   cat * \
   | (
-    exec 2>/dev/null;
     cpp -fpreprocessed || gcpp -fpreprocessed
   ) | (
     x=`sed 's/[_a-zA-Z0-9][_a-zA-Z0-9]*/x/g' | tr -d ' \012' | wc -c | tr -d ' '`
-    echo "=== `date` ===   $x words of code"
+    log2 "$x words of code"
   )
 )
-echo "=== `date` === finishing"
+log1 "finishing"
 
 
 echo "=== `date` === counting words of code"
-rm -rf "$work"
-mkdir -p "$work"
+rm -rf "${work}"
+mkdir -p "${work}"
 
 for dir in sysdep tinyssh crypto; do
   (
-    exec 2>/dev/null
     cd "${dir}"
     cat *.c *.h > "${work}/${dir}" || :
   )
@@ -334,11 +344,10 @@ for dir in sysdep tinyssh crypto; do
     cd "${work}"
     cat "${dir}" \
     | (
-      exec 2>/dev/null; 
       cpp -fpreprocessed || gcpp -fpreprocessed
     ) | (
       x=`sed 's/[_a-zA-Z0-9][_a-zA-Z0-9]*/x/g' | tr -d ' \012' | wc -c | tr -d ' '`
-      echo "=== `date` ===   ${dir} $x"
+      log2 "${dir} ${x}"
     )
   )
 done
@@ -346,12 +355,11 @@ done
   cd "${work}"
   cat * \
   | (
-    exec 2>/dev/null;
     cpp -fpreprocessed || gcpp -fpreprocessed
   ) | (
     x=`sed 's/[_a-zA-Z0-9][_a-zA-Z0-9]*/x/g' | tr -d ' \012' | wc -c | tr -d ' '`
-    echo "=== `date` ===   $x words of code"
+    log2 "${x} words of code"
   )
 )
-echo "=== `date` === finishing"
+log1 "finishing"
 exit 0
