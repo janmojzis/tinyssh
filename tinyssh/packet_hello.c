cf7e8f1e6092f5666a1dd9a54a9cdab8d0ff7868
Jan Mojzis <github@mojzis.com>
2016-02-26 06:52:02 +0100

    corrected log messages, added tests

--- tinyssh/packet_hello.c
+++ tinyssh/packet_hello.c
@@ -9,15 +9,15 @@ Public domain.
 #include "writeall.h"
 #include "purge.h"
 #include "log.h"
-#include "bug.h"
 #include "str.h"
 #include "getln.h"
+#include "e.h"
 #include "packet.h"
 
 /*
 The 'packet_hello_send()' function sends SSH hello string to the client.
 See RFC 4253 4.2. Protocol Version Exchange.
-Example: SSH-2.0-tinyssh_20140501experimental 65f8E84J
+Example: SSH-2.0-tinyssh_20160201 iX512l8j
 */
 int packet_hello_send(void) {
 
@@ -32,7 +32,7 @@ int packet_hello_send(void) {
     buf_puts(b, " ");
     buf_puts(b, log_string());
     buf_puts(b, "\r\n");
-    if (writeall(1, b->buf, b->len) == -1) bug();
+    if (writeall(1, b->buf, b->len) == -1) { log_w1("hello: write problem"); return 0; }
     b->len -= 2; /* remove "\r\n" */
     b->buf[b->len] = 0;
     log_d2("hello: server: ", (char *)(b->buf));
@@ -51,13 +51,14 @@ int packet_hello_receive(void) {
     struct buf *b = &packet.helloreceive;
 
     r = getln(0, b->buf, b->alloc);
-    if (r != 1) bug();
+    if (r == 0) { errno = ECONNRESET; return 0; }
+    if (r != 1) return 0;
     b->len = str_len((char *)b->buf);
-    if (b->len < 6) bug_proto();
+    if (b->len < 6) { errno = EPROTO; return 0; }
     if (b->buf[b->len - 1] == '\n') --(b->len); /* remove '\n' */
     if (b->buf[b->len - 1] == '\r') --(b->len); /* remove '\r' */
-    if (!byte_isequal(b->buf, 4, "SSH-")) bug_proto();
     b->buf[b->len] = 0;
+    if (!byte_isequal(b->buf, 4, "SSH-")) { errno = EPROTO; return 0; }
     log_d2("hello: client: ", (char *)b->buf);
     purge(b->buf + b->len, b->alloc - b->len);
     return 1;
