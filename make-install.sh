3c68781d3de0c9e2b54869bf155df2120f098c58
Les Aker <me@lesaker.org>
2016-07-27 10:47:26 -0400

    Adjust make-install.sh to create numbered mandir

--- make-install.sh
+++ make-install.sh
@@ -39,11 +39,12 @@ echo "=== `date` === finishing"
 confman="`head -1 conf-man`"
 mandir=`echo "$1/${confman}" | sed 's,//,/,g'`
 echo "=== `date` === installing man directory ${mandir}"
-mkdir -p "${bindir}" || exit 111
+mkdir -p "${mandir}" || exit 111
 ls "${man}" | sort |\
 while read x
 do
   n=`echo "${x}" | cut -d'.' -f2`
+  mkdir -p "${mandir}/man${n}" || exit 111
   "${installcmd}" "${man}/${x}" "${mandir}/man${n}" "${x}.tmp" "${x}" || exit 111
   echo "=== `date` ===   installing ${man}/${x} -> ${mandir}/man${n}/${x}"
 done || exit 111
