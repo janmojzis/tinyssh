778562b0a3fe269e54e39376ca7edc7df89c3cfb
Jan Mojzis <jan.mojzis@gmail.com>
2016-08-27 20:56:32 +0200

    removed little/big-endian detection

--- crypto/uint32_unpack.c
+++ crypto/uint32_unpack.c
@@ -1,4 +1,3 @@
-#include "order.h"
 #include "uint32_unpack.h"
 
 /*
@@ -7,12 +6,8 @@ in little-endian format into 32-bit unsigned integer.
 */
 crypto_uint32 uint32_unpack(const unsigned char *x) {
 
-#ifdef ORDER_LITTLEENDIAN
-    return *(crypto_uint32 *)x;
-#else
     crypto_uint32 y = 0;
     long long i;
     for (i = 3; i >= 0; --i) y = (y << 8) | x[i];
     return y;
-#endif
 }
