6030a78041882f8c2177b76d6d3cfa9d1b12a698
Jan Mojzis <jan.mojzis@gmail.com>
2018-01-04 21:43:39 +0100

    sha512 - added full processor independent impl.

--- crypto/sha512.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SHA512_H____
-#define _SHA512_H____
-
-#include "crypto_uint64.h"
-
-extern void sha512_hash(unsigned char *, const unsigned char *, unsigned long long, crypto_uint64 *);
-
-#endif
