4b10348f1e16fa6f130f5e08988d8ed4bceb269e
Jan Mojzis <jan.mojzis@gmail.com>
2018-01-04 21:30:08 +0100

    chacha20 - removed little-endian optimized implementation, added full processor independent implementation

--- crypto/crypto_stream_chacha20.c
+++ crypto/crypto_stream_chacha20.c
@@ -1,21 +1,19 @@
 /*
-20170509
+20180104
 Jan Mojzis
 Public domain.
 */
 
-#include "crypto_uint32.h"
-#include "crypto_uint64.h"
 #include "crypto_stream_chacha20.h"
 
-static crypto_uint32 unpack32(const unsigned char *x) {
+static unsigned long unpack32(const unsigned char *x) {
     return
-        (crypto_uint32) (x[0])                  \
-    | (((crypto_uint32) (x[1])) << 8)           \
-    | (((crypto_uint32) (x[2])) << 16)          \
-    | (((crypto_uint32) (x[3])) << 24);
+        (unsigned long) (x[0])                  \
+    | (((unsigned long) (x[1])) << 8)           \
+    | (((unsigned long) (x[2])) << 16)          \
+    | (((unsigned long) (x[3])) << 24);
 }
-static void pack32(unsigned char *x, crypto_uint32 u) {
+static void pack32(unsigned char *x, unsigned long u) {
     x[0] = u; u >>= 8;
     x[1] = u; u >>= 8;
     x[2] = u; u >>= 8;
@@ -86,58 +84,73 @@ static void pack32(unsigned char *x, crypto_uint32 u) {
     pack32(o + 56, (x14 + n2) ^ unpack32(i + 56));  \
     pack32(o + 60, (x15 + n3) ^ unpack32(i + 60));
 
+#define _CRYPTO_STREAM_XOR(inttype, fcename)        \
+static int fcename( unsigned char *c,               \
+                    const unsigned char *m,         \
+                    unsigned long long l,           \
+                    const unsigned char *n,         \
+                    const unsigned char *k) {       \
+                                                    \
+    inttype x0, x1, x2, x3, x4, x5, x6, x7;         \
+    inttype x8, x9, x10, x11, x12, x13, x14, x15;   \
+    inttype k0 = unpack32(k     );                  \
+    inttype k1 = unpack32(k +  4);                  \
+    inttype k2 = unpack32(k +  8);                  \
+    inttype k3 = unpack32(k + 12);                  \
+    inttype k4 = unpack32(k + 16);                  \
+    inttype k5 = unpack32(k + 20);                  \
+    inttype k6 = unpack32(k + 24);                  \
+    inttype k7 = unpack32(k + 28);                  \
+    inttype n0 = 0;                                 \
+    inttype n1 = 0;                                 \
+    inttype n2 = unpack32(n    );                   \
+    inttype n3 = unpack32(n + 4);                   \
+    inttype s0 = 0x61707865;                        \
+    inttype s1 = 0x3320646E;                        \
+    inttype s2 = 0x79622D32;                        \
+    inttype s3 = 0x6B206574;                        \
+    unsigned long long u = 0;                       \
+                                                    \
+    if (!l) return 0;                               \
+                                                    \
+    while (l >= 64) {                               \
+        XORBLOCK(c, m);                             \
+                                                    \
+        ++u;                                        \
+        if (!u) return -1;                          \
+        n0 = (inttype)(u & 0xffffffff);             \
+        n1 = (inttype)(u >> 32);                    \
+                                                    \
+        l -= 64;                                    \
+        c += 64;                                    \
+        m += 64;                                    \
+    }                                               \
+    if (l) {                                        \
+        unsigned char b[64] = {0};                  \
+        long long j;                                \
+                                                    \
+        for (j = 0; j < l; ++j) b[j] = m[j];        \
+        XORBLOCK(b, b);                             \
+        for (j = 0; j < l; ++j) c[j] = b[j];        \
+    }                                               \
+    return 0;                                       \
+}
+
+_CRYPTO_STREAM_XOR(unsigned int, int_crypto_stream_xor)
+_CRYPTO_STREAM_XOR(unsigned long, long_crypto_stream_xor)
 
 int crypto_stream_chacha20_tinynacl_xor(unsigned char *c, const unsigned char *m, unsigned long long l, const unsigned char *n, const unsigned char *k) {
-
-    crypto_uint32 x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15;
-    crypto_uint32 k0 = unpack32(k     );
-    crypto_uint32 k1 = unpack32(k +  4);
-    crypto_uint32 k2 = unpack32(k +  8);
-    crypto_uint32 k3 = unpack32(k + 12);
-    crypto_uint32 k4 = unpack32(k + 16);
-    crypto_uint32 k5 = unpack32(k + 20);
-    crypto_uint32 k6 = unpack32(k + 24);
-    crypto_uint32 k7 = unpack32(k + 28);
-    crypto_uint32 n0 = 0;
-    crypto_uint32 n1 = 0;
-    crypto_uint32 n2 = unpack32(n    );
-    crypto_uint32 n3 = unpack32(n + 4);
-    crypto_uint32 s0 = 0x61707865;
-    crypto_uint32 s1 = 0x3320646E;
-    crypto_uint32 s2 = 0x79622D32;
-    crypto_uint32 s3 = 0x6B206574;
-    crypto_uint64 u = 0;
-
-    if (!l) return 0;
-
-    while (l >= 64) {
-        XORBLOCK(c, m);
-
-        ++u;
-        if (!u) return -1;
-        n0 = (crypto_uint32)(u & 0xffffffff);
-        n1 = (crypto_uint32)(u >> 32);
-
-        l -= 64;
-        c += 64;
-        m += 64;
-    }
-    if (l) {
-        unsigned char bspace[64] = {0};
-        unsigned char *b = bspace;
-        long long j;
-
-        for (j = 0; j < l; ++j) b[j] = m[j];
-        XORBLOCK(b, b);
-        for (j = 0; j < l; ++j) c[j] = b[j];
-    }
-    return 0;
+    if (sizeof(int) == 4) return int_crypto_stream_xor(c, m, l, n, k);
+    return long_crypto_stream_xor(c, m, l, n, k);
 }
 
 int crypto_stream_chacha20_tinynacl(unsigned char *c, unsigned long long l, const unsigned char *n, const unsigned char *k) {
 
     long long j;
+    unsigned char ncopy[8], kcopy[32];
 
-    for (j = 0; j < l; ++j) c[j] = 0;
-    return crypto_stream_chacha20_tinynacl_xor(c, c, l, n, k);
+    for (j = 0; j < 32; ++j) kcopy[j] = k[j];
+    for (j = 0; j <  8; ++j) ncopy[j] = n[j];
+    for (j = 0; j <  l; ++j) c[j] = 0;
+    return crypto_stream_chacha20_tinynacl_xor(c, c, l, ncopy, kcopy);
 }
