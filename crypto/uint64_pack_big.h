cdc95b904ed18fac29205ba9b6bc351cd9853820
Jan Mojzis <jan.mojzis@gmail.com>
2018-01-09 06:48:44 +0100

    removed uint64_pack_big/uint64_unpack_big

--- crypto/uint64_pack_big.h
+++
@@ -1,8 +0,0 @@
-#ifndef _UINT64_PACK_BIG____
-#define _UINT64_PACK_BIG____
-
-#include "crypto_uint64.h"
-
-extern void uint64_pack_big(unsigned char *y, crypto_uint64 x);
-
-#endif
--- crypto/uint64_unpack_big.h
+++
@@ -1,8 +0,0 @@
-#ifndef _UINT64_UNPACK_BIG____
-#define _UINT64_UNPACK_BIG____
-
-#include "crypto_uint64.h"
-
-extern crypto_uint64 uint64_unpack_big(const unsigned char *);
-
-#endif
