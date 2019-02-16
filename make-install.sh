656a6b0313286bfe097c222a91b3a4b51b795b5a
Jan Mojžíš <jan.mojzis@gmail.com>
2019-02-16 18:20:16 +0100

    make-install.sh rework, removed _tinysshd-install

--- make-install.sh
+++ make-install.sh
@@ -4,23 +4,16 @@ build="`pwd`/build"
 source="`pwd`"
 bin="${build}/bin"
 man="${build}/man"
-installcmd="${bin}/_tinysshd-install"
 
 cat "${source}/tinyssh/TARGETS" |\
 while read x
 do
   [ -x "${bin}/${x}" ] || \
-    ( 
+    (
       echo "=== `date` === $x not compiled, compile first!"
-      exit 111; 
+      exit 111;
     ) || exit 111
 done || exit 111
-
-[ -x "${installcmd}" ] || \
-    ( 
-      echo "=== `date` === ${installcmd} not compiled, compile first!"
-      exit 111; 
-    ) || exit 111
 
 #bin
 confbin="`head -1 conf-bin`"
@@ -30,7 +23,12 @@ mkdir -p "${bindir}" || exit 111
 cat "${source}/tinyssh/TARGETS" |\
 while read x
 do
-  "${installcmd}" "${bin}/${x}" "${bindir}" "${x}.tmp" "${x}" 1 || exit 111
+  cp "${bin}/${x}" "${bindir}/${x}.tmp"
+  (
+    cd "${bindir}"
+    chmod 755 "${x}.tmp"
+    mv -f "${x}.tmp" "${x}"
+  )
   echo "=== `date` ===   installing ${bin}/${x} -> ${bindir}/${x}"
 done || exit 111
 echo "=== `date` === finishing"
@@ -45,7 +43,12 @@ while read x
 do
   n=`echo "${x}" | cut -d'.' -f2`
   mkdir -p "${mandir}/man${n}" || exit 111
-  "${installcmd}" "${man}/${x}" "${mandir}/man${n}" "${x}.tmp" "${x}" || exit 111
+  cp "${man}/${x}" "${mandir}/man${n}/${x}.tmp"
+  (
+    cd "${mandir}/man${n}"
+    chmod 644 "${x}.tmp"
+    mv -f "${x}.tmp" "${x}"
+  )
   echo "=== `date` ===   installing ${man}/${x} -> ${mandir}/man${n}/${x}"
 done || exit 111
 echo "=== `date` === finishing"
