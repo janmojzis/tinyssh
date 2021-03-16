f55ddad261eb5acb7ff04a328b4e1063c07ef52f
Jan Mojžíš <jan.mojzis@gmail.com>
2021-03-16 04:31:20 +0100

    crypto/crypto_sort_uint32.c uint32_t -> crypto_uint32

--- crypto/crypto_sort_uint32.c
+++ crypto/crypto_sort_uint32.c
@@ -1,12 +1,12 @@
-#include <stdint.h>
+#include "crypto_uint32.h"
 #include "crypto_sort_uint32.h"
 
-static void minmax(uint32_t *x, uint32_t *y) {
+static void minmax(crypto_uint32 *x, crypto_uint32 *y) {
 
-    uint32_t xi = *x;
-    uint32_t yi = *y;
-    uint32_t xy = xi ^ yi;
-    uint32_t c = yi - xi;
+    crypto_uint32 xi = *x;
+    crypto_uint32 yi = *y;
+    crypto_uint32 xy = xi ^ yi;
+    crypto_uint32 c = yi - xi;
 
     c ^= xy & (c ^ yi ^ 0x80000000);
     c >>= 31;
@@ -20,7 +20,7 @@ static void minmax(uint32_t *x, uint32_t *y) {
 void crypto_sort_uint32_tinynacl(void *xv, long long n) {
 
     long long top, p, q, i;
-    uint32_t *x = xv;
+    crypto_uint32 *x = xv;
 
     if (n < 2) return;
     top = 1;
@@ -30,9 +30,9 @@ void crypto_sort_uint32_tinynacl(void *xv, long long n) {
         for (i = 0; i < n - p; ++i)
             if (!(i & p))
                 minmax(x + i, x + i + p);
-    for (q = top;q > p;q >>= 1)
-        for (i = 0; i < n - q; ++i)
-            if (!(i & p))
-                minmax(x + i + p, x + i + q);
+        for (q = top; q > p; q >>= 1)
+            for (i = 0; i < n - q; ++i)
+                if (!(i & p))
+                    minmax(x + i + p, x + i + q);
     }
 }
