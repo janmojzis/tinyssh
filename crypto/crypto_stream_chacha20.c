f5ae8e7f436dd98b9f4fb79e7b62ac4144e870a1
Jan Mojžíš <jan.mojzis@gmail.com>
2022-02-22 06:44:47 +0100

    crypto_stream_chacha20.c update

--- crypto/crypto_stream_chacha20.c
+++ crypto/crypto_stream_chacha20.c
@@ -1,32 +1,34 @@
 /*
-20180104
+20210508
 Jan Mojzis
 Public domain.
 */
 
+#include <stdint.h>
 #include "crypto_stream_chacha20.h"
 
-static unsigned long unpack32(const unsigned char *x) {
+/* clang-format off */
+static inline uint32_t unpack32(const unsigned char *x) {
     return
-        (unsigned long) (x[0])                  \
-    | (((unsigned long) (x[1])) << 8)           \
-    | (((unsigned long) (x[2])) << 16)          \
-    | (((unsigned long) (x[3])) << 24);
+        (uint32_t) (x[0])                  \
+    | (((uint32_t) (x[1])) << 8)           \
+    | (((uint32_t) (x[2])) << 16)          \
+    | (((uint32_t) (x[3])) << 24);
 }
-static void pack32(unsigned char *x, unsigned long u) {
+static inline void pack32(unsigned char *x, uint32_t u) {
     x[0] = u; u >>= 8;
     x[1] = u; u >>= 8;
     x[2] = u; u >>= 8;
     x[3] = u;
 }
 
-#define ROTATE32(x, c) ((x) << (c)) | (((x) & 0xffffffff) >> (32 - (c)))
+#define ROTATE(x, c) ((x) << (c)) ^ ((x) >> (32 - (c)))
 
 #define QUARTERROUND(a, b, c, d)                    \
-    a += b; d = ROTATE32(d ^ a, 16);                \
-    c += d; b = ROTATE32(b ^ c, 12);                \
-    a += b; d = ROTATE32(d ^ a,  8);                \
-    c += d; b = ROTATE32(b ^ c,  7);
+    a += b; d = ROTATE(d ^ a, 16);                  \
+    c += d; b = ROTATE(b ^ c, 12);                  \
+    a += b; d = ROTATE(d ^ a,  8);                  \
+    c += d; b = ROTATE(b ^ c,  7);
 
 #define TWOROUNDS                                   \
     QUARTERROUND( x0, x4,  x8, x12)                 \
@@ -84,69 +86,55 @@ static void pack32(unsigned char *x, unsigned long u) {
     pack32(o + 56, (x14 + n2) ^ unpack32(i + 56));  \
     pack32(o + 60, (x15 + n3) ^ unpack32(i + 60));
 
-#define _CRYPTO_STREAM_XOR(inttype, fcename)        \
-static int fcename( unsigned char *c,               \
-                    const unsigned char *m,         \
-                    unsigned long long l,           \
-                    const unsigned char *n,         \
-                    const unsigned char *k) {       \
-                                                    \
-    inttype x0, x1, x2, x3, x4, x5, x6, x7;         \
-    inttype x8, x9, x10, x11, x12, x13, x14, x15;   \
-    inttype k0 = unpack32(k     );                  \
-    inttype k1 = unpack32(k +  4);                  \
-    inttype k2 = unpack32(k +  8);                  \
-    inttype k3 = unpack32(k + 12);                  \
-    inttype k4 = unpack32(k + 16);                  \
-    inttype k5 = unpack32(k + 20);                  \
-    inttype k6 = unpack32(k + 24);                  \
-    inttype k7 = unpack32(k + 28);                  \
-    inttype n0 = 0;                                 \
-    inttype n1 = 0;                                 \
-    inttype n2 = unpack32(n    );                   \
-    inttype n3 = unpack32(n + 4);                   \
-    inttype s0 = 0x61707865;                        \
-    inttype s1 = 0x3320646E;                        \
-    inttype s2 = 0x79622D32;                        \
-    inttype s3 = 0x6B206574;                        \
-    unsigned long long u = 0;                       \
-                                                    \
-    if (!l) return 0;                               \
-                                                    \
-    while (l >= 64) {                               \
-        XORBLOCK(c, m);                             \
-                                                    \
-        ++u;                                        \
-        if (!u) return -1;                          \
-        n0 = (inttype)(u & 0xffffffff);             \
-        n1 = (inttype)(u >> 32);                    \
-                                                    \
-        l -= 64;                                    \
-        c += 64;                                    \
-        m += 64;                                    \
-    }                                               \
-    if (l) {                                        \
-        unsigned char b[64] = {0};                  \
-        long long j;                                \
-                                                    \
-        for (j = 0; j < l; ++j) b[j] = m[j];        \
-        XORBLOCK(b, b);                             \
-        for (j = 0; j < l; ++j) c[j] = b[j];        \
-    }                                               \
-    return 0;                                       \
-}
-
-_CRYPTO_STREAM_XOR(unsigned int, int_crypto_stream_xor)
-_CRYPTO_STREAM_XOR(unsigned long, long_crypto_stream_xor)
-
 int crypto_stream_chacha20_tinynacl_xor(unsigned char *c, const unsigned char *m, unsigned long long l, const unsigned char *n, const unsigned char *k) {
-    if (sizeof(int) == 4) return int_crypto_stream_xor(c, m, l, n, k);
-    return long_crypto_stream_xor(c, m, l, n, k);
+
+    register uint32_t x0, x1, x2, x3, x4, x5, x6, x7;
+    register uint32_t x8, x9, x10, x11, x12, x13, x14, x15;
+    uint32_t k0 = unpack32(k     );
+    uint32_t k1 = unpack32(k +  4);
+    uint32_t k2 = unpack32(k +  8);
+    uint32_t k3 = unpack32(k + 12);
+    uint32_t k4 = unpack32(k + 16);
+    uint32_t k5 = unpack32(k + 20);
+    uint32_t k6 = unpack32(k + 24);
+    uint32_t k7 = unpack32(k + 28);
+    uint32_t n0 = 0;
+    uint32_t n1 = 0;
+    uint32_t n2 = unpack32(n    );
+    uint32_t n3 = unpack32(n + 4);
+    uint32_t s0 = 0x61707865;
+    uint32_t s1 = 0x3320646E;
+    uint32_t s2 = 0x79622D32;
+    uint32_t s3 = 0x6B206574;
+    uint64_t u = 0;
+
+    if (!l) return 0;
+
+    while (l >= 64) {
+        XORBLOCK(c, m);
+
+        if (!++u) return -1;
+        n0 = u;
+        n1 = u >> 32;
+
+        l -= 64;
+        c += 64;
+        m += 64;
+    }
+    if (l) {
+        unsigned char b[64] = {0};
+        unsigned long long j;
+
+        for (j = 0; j < l; ++j) b[j] = m[j];
+        XORBLOCK(b, b);
+        for (j = 0; j < l; ++j) c[j] = b[j];
+    }
+    return 0;
 }
 
 int crypto_stream_chacha20_tinynacl(unsigned char *c, unsigned long long l, const unsigned char *n, const unsigned char *k) {
 
-    long long j;
+    unsigned long long j;
     unsigned char ncopy[8], kcopy[32];
 
     for (j = 0; j < 32; ++j) kcopy[j] = k[j];
@@ -154,3 +142,4 @@ int crypto_stream_chacha20_tinynacl(unsigned char *c, unsigned long long l, cons
     for (j = 0; j <  l; ++j) c[j] = 0;
     return crypto_stream_chacha20_tinynacl_xor(c, c, l, ncopy, kcopy);
 }
+/* clang-format on */
