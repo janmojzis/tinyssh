c90331dc70a4ab725b29cfbf8f291605a4761021
Jan Mojžíš <jan.mojzis@gmail.com>
2024-11-12 18:52:58 +0100

    coe.c: clang-format

--- coe.c
+++ coe.c
@@ -1,7 +1,5 @@
 /*
-20140414
-Jan Mojzis
-Public domain.
+20241112
 */
 
 #include <fcntl.h>
@@ -11,14 +9,10 @@ Public domain.
 The 'coe_enable(fd)' function enables
 the close-on-exec flag on a file-descriptor 'fd'.
 */
-void coe_enable(int fd) {
-    fcntl(fd, F_SETFD, 1);
-}
+void coe_enable(int fd) { fcntl(fd, F_SETFD, 1); }
 
 /*
 The 'coe_disable(fd)' function disables
 the close-on-exec flag on a file-descriptor 'fd'.
 */
-void coe_disable(int fd) {
-    fcntl(fd, F_SETFD, 0);
-}
+void coe_disable(int fd) { fcntl(fd, F_SETFD, 0); }
