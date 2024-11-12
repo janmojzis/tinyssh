f9201fb5c6d3983a13923334b502c9160afb92f5
Jan Mojžíš <jan.mojzis@gmail.com>
2024-11-12 18:50:06 +0100

    writeall.c: fix [-Wsign-conversion], clang-format

--- writeall.c
+++ writeall.c
@@ -1,29 +1,28 @@
-/* taken from nacl-20110221, from curvecp/writeall.c */
-#include <poll.h>
 #include <unistd.h>
+#include <poll.h>
 #include "e.h"
 #include "writeall.h"
 
-int writeall(int fd,const void *xv,long long xlen)
-{
-  char *x = (char *)xv;
-  long long w;
-  while (xlen > 0) {
-    w = xlen;
-    if (w > 1048576) w = 1048576;
-    w = write(fd,x,w);
-    if (w < 0) {
-      if (errno == EINTR || errno == EAGAIN || errno == EWOULDBLOCK) {
-        struct pollfd p;
-        p.fd = fd;
-        p.events = POLLOUT | POLLERR;
-        poll(&p,1,-1);
-        continue;
-      }
-      return -1;
+int writeall(int fd, const void *xv, long long xlen) {
+
+    const unsigned char *x = xv;
+    long long w;
+    while (xlen > 0) {
+        w = xlen;
+        if (w > 1048576) w = 1048576;
+        w = write(fd, x, (unsigned long long) w);
+        if (w < 0) {
+            if (errno == EINTR || errno == EAGAIN || errno == EWOULDBLOCK) {
+                struct pollfd p;
+                p.fd = fd;
+                p.events = POLLOUT | POLLERR;
+                poll(&p, 1, -1);
+                continue;
+            }
+            return -1;
+        }
+        x += w;
+        xlen -= w;
     }
-    x += w;
-    xlen -= w;
-  }
-  return 0;
+    return 0;
 }
