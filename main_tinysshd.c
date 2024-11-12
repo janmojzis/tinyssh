85464c1ca41bd814b80c7d62ce86f36494e1be80
Jan Mojžíš <jan.mojzis@gmail.com>
2024-11-12 06:23:58 +0100

    main_tinysshd.c: fix compiler warning [-Wunused-result]

--- main_tinysshd.c
+++ main_tinysshd.c
@@ -46,9 +46,9 @@ static void timeout(int x) {
 static int selfpipe[2] = { -1, -1 };
 
 static void trigger(int x) {
-    (void) x;
     errno = 0;
-    write(selfpipe[1], "", 1);
+    x = write(selfpipe[1], "", 1);
+    (void) x;
 }
 
 int main_tinysshd(int argc, char **argv, const char *binaryname) {
