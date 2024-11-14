08b71022d943f97b34abc59df4c31a34c6868ad6
Jan Mojžíš <jan.mojzis@gmail.com>
2024-11-14 11:17:10 +0100

    crypto_sort_uint32.h add crypto_sort_uint32_BYTES

--- crypto_sort_uint32.h
+++ crypto_sort_uint32.h
@@ -1,9 +1,11 @@
 #ifndef crypto_sort_uint32_H
 #define crypto_sort_uint32_H
 
+#define crypto_sort_uint32_tinyssh_BYTES 4
 extern void crypto_sort_uint32_tinyssh(void *, long long);
 
 #define crypto_sort_uint32 crypto_sort_uint32_tinyssh
+#define crypto_sort_uint32_BYTES crypto_sort_uint32_tinyssh_BYTES
 #define crypto_sort_uint32_IMPLEMENTATION "tinyssh"
 #define crypto_sort_uint32_VERSION "-"
 
