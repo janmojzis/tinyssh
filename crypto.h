b564dc3cad75830da6781a4c226dc3dd121a853b
Jan Mojžíš <jan.mojzis@gmail.com>
2024-11-16 07:30:25 +0100

    crypto.h: add ext. randombytes.h if available

--- crypto.h
+++ crypto.h
@@ -1,9 +1,26 @@
-#include "crypto_hash_sha256.h"
-#include "crypto_hash_sha512.h"
+/* allways include local versions */
 #include "crypto_int16.h"
 #include "crypto_int32.h"
 #include "crypto_int64.h"
 #include "crypto_int8.h"
+#include "crypto_uint16.h"
+#include "crypto_uint32.h"
+#include "crypto_uint64.h"
+#include "crypto_uint8.h"
+#include "crypto_verify_16.h"
+#include "crypto_verify_32.h"
+
+/* include randombytes from librandombytes */
+#include "haslibrandombytes.h"
+#ifdef HASLIBRANDOMBYTES
+#include <randombytes.h>
+#else
+#include "randombytes.h"
+#endif
+
+/* TODO */
+#include "crypto_hash_sha256.h"
+#include "crypto_hash_sha512.h"
 #include "crypto_kem_sntrup761.h"
 #include "crypto_kem_sntrup761x25519.h"
 #include "crypto_onetimeauth_poly1305.h"
@@ -11,10 +28,3 @@
 #include "crypto_sign_ed25519.h"
 #include "crypto_sort_uint32.h"
 #include "crypto_stream_chacha20.h"
-#include "crypto_uint16.h"
-#include "crypto_uint32.h"
-#include "crypto_uint64.h"
-#include "crypto_uint8.h"
-#include "crypto_verify_16.h"
-#include "crypto_verify_32.h"
-#include "randombytes.h"
