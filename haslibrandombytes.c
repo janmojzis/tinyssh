8de70088f6cf9b1e37f6ddb05fec90563d3fcf87
Jan Mojžíš <jan.mojzis@gmail.com>
2024-11-16 05:29:32 +0100

    haslibrandombytes.c: check also randombytes_source() function

--- haslibrandombytes.c
+++ haslibrandombytes.c
@@ -12,17 +12,20 @@ static unsigned char z(unsigned char x) {
         t = (t * z) % 257;
     }
     t = (t * z) % 257;
-    return t - 1;
+    return (unsigned char) t - 1;
 }
 
 int main(int argc, char **argv) {
     unsigned char buf[32], ret = 0;
-    long long i;
+    const char *source;
+    unsigned long long i;
 
     (void) argc;
     (void) argv;
 
     randombytes(buf, sizeof buf);
-    for (i = 0; i < sizeof buf; ++i) ret ^= buf[i];
+    for (i = 0; i < sizeof buf; ++i) ret |= z(buf[i]);
+    source = randombytes_source();
+    for (i = 0; source[i]; ++i) ret |= z((unsigned char) source[i]);
     return z(ret);
 }
