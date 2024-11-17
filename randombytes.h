e35ddbc44c8b339e91c1a153a94835bd69192065
Jan Mojžíš <jan.mojzis@gmail.com>
2024-11-17 18:57:06 +0100

    randombytes.h: randombytes is now macro compatible with librandombytes

--- randombytes.h
+++ randombytes.h
@@ -5,6 +5,8 @@
 #ifdef HASLIBRANDOMBYTES
 #include <randombytes.h>
 #else
+#define randombytes randombytes_internal_void_voidstar_longlong
+#define randombytes_source randombytes_internal_source
 extern void randombytes(void *, long long);
 extern const char *randombytes_source(void);
 #endif
