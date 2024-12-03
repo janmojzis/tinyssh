6346072aad67fee77f1cba99a58a94eac56a6f1d
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-03 21:11:47 +0100

    byte.[ch]: rework, switch to cryptoint, clang-format

--- byte.c
+++ byte.c
@@ -1,24 +1,37 @@
-/* taken from nacl-20110221, from curvecp/byte_copy.c curvecp/byte_isequal.c curvecp/byte_zero.c */
+/*
+20241205
+*/
+
+#include "crypto_int16.h"
 #include "byte.h"
 
-void byte_copy(void *yv,long long ylen,const void *xv)
-{
-  char *y = (char *)yv;
-  const char *x = (const char *)xv;
-  while (ylen > 0) { *y++ = *x++; --ylen; }
+void byte_copy(void *yv, long long ylen, const void *xv) {
+
+    long long i;
+    const char *x = xv;
+    char *y = yv;
+
+    for (i = 0; i < ylen; ++i) y[i] = x[i];
 }
 
-int byte_isequal(const void *yv,long long ylen,const void *xv)
-{
-  const unsigned char *y = (const unsigned char *)yv;
-  const unsigned char *x = (const unsigned char *)xv;
-  unsigned char diff = 0;
-  while (ylen > 0) { diff |= (*y++ ^ *x++); --ylen; }
-  return (256 - (unsigned int) diff) >> 8;
+int byte_isequal(const void *yv, long long ylen, const void *xv) {
+
+    long long i;
+    const unsigned char *y = yv;
+    const unsigned char *x = xv;
+    unsigned char diff = 0;
+
+    for (i = 0; i < ylen; ++i) diff |= x[i] ^ y[i];
+    return crypto_int16_zero_01(diff);
 }
 
-void byte_zero(void *yv,long long ylen)
-{
-  char *y = (char *)yv;
-  while (ylen > 0) { *y++ = 0; --ylen; }
+void byte_zero(void *yv, long long ylen) {
+
+    long long i;
+    volatile char *y = yv;
+
+    for (i = 0; i < ylen; ++i) y[i] = 0;
+#ifdef __GNUC__
+    __asm__ __volatile__("" : : "r"(yv) : "memory");
+#endif
 }
--- byte.h
+++ byte.h
@@ -1,8 +1,8 @@
-#ifndef BYTE_H
-#define BYTE_H
+#ifndef BYTE_H____
+#define BYTE_H____
 
-extern void byte_zero(void *,long long);
-extern void byte_copy(void *,long long,const void *);
-extern int byte_isequal(const void *,long long,const void *);
+extern void byte_zero(void *, long long);
+extern void byte_copy(void *, long long, const void *);
+extern int byte_isequal(const void *, long long, const void *);
 
 #endif
