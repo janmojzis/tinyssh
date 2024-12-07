93a11960cacc618745ec38d0950a342b85cde47e
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-07 18:58:12 +0100

    writeall.h: clang-format

--- writeall.h
+++ writeall.h
@@ -1,6 +1,6 @@
-#ifndef WRITEALL_H
-#define WRITEALL_H
+#ifndef WRITEALL_H____
+#define WRITEALL_H____
 
-extern int writeall(int,const void *,long long);
+extern int writeall(int, const void *, long long);
 
 #endif
