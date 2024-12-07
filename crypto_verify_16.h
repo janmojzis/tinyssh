b09b5b630122885534b0a1ce56bafa342aa20608
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-07 18:56:11 +0100

    crypto_verify_{16,32}.h: clang-format

--- crypto_verify_16.h
+++ crypto_verify_16.h
@@ -2,7 +2,8 @@
 #define crypto_verify_16_H
 
 #define crypto_verify_16_tinyssh_BYTES 16
-extern int crypto_verify_16_tinyssh(const unsigned char *, const unsigned char *);
+extern int crypto_verify_16_tinyssh(const unsigned char *,
+                                    const unsigned char *);
 
 #define crypto_verify_16 crypto_verify_16_tinyssh
 #define crypto_verify_16_BYTES crypto_verify_16_tinyssh_BYTES
--- crypto_verify_32.h
+++ crypto_verify_32.h
@@ -2,7 +2,8 @@
 #define crypto_verify_32_H
 
 #define crypto_verify_32_tinyssh_BYTES 32
-extern int crypto_verify_32_tinyssh(const unsigned char *, const unsigned char *);
+extern int crypto_verify_32_tinyssh(const unsigned char *,
+                                    const unsigned char *);
 
 #define crypto_verify_32 crypto_verify_32_tinyssh
 #define crypto_verify_32_BYTES crypto_verify_32_tinyssh_BYTES
