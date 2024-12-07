f2fc594a9f4d35d02c193205ca106c12412c55aa
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-07 17:02:41 +0100

    blocking.[ch]: clang-format

--- blocking.c
+++ blocking.c
@@ -1,13 +1,15 @@
-/* taken from nacl-20110221, from curvecp/blocking.c */
+/*
+taken from nacl-20110221, from from curvecp/blocking.c
+- reformated using clang-format
+*/
+
 #include <fcntl.h>
 #include "blocking.h"
 
-void blocking_enable(int fd)
-{
-  fcntl(fd,F_SETFL,fcntl(fd,F_GETFL,0) & ~O_NONBLOCK);
+void blocking_enable(int fd) {
+    fcntl(fd, F_SETFL, fcntl(fd, F_GETFL, 0) & ~O_NONBLOCK);
 }
 
-void blocking_disable(int fd)
-{
-  fcntl(fd,F_SETFL,fcntl(fd,F_GETFL,0) | O_NONBLOCK);
+void blocking_disable(int fd) {
+    fcntl(fd, F_SETFL, fcntl(fd, F_GETFL, 0) | O_NONBLOCK);
 }
--- blocking.h
+++ blocking.h
@@ -1,5 +1,5 @@
-#ifndef BLOCKING_H
-#define BLOCKING_H
+#ifndef BLOCKING_H____
+#define BLOCKING_H____
 
 extern void blocking_enable(int);
 extern void blocking_disable(int);
