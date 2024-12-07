aa14ff0176adde6d1d7820f50f887d00f4b8754e
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-07 18:51:59 +0100

    porttostr.c: clang-format

--- porttostr.c
+++ porttostr.c
@@ -1,5 +1,6 @@
 /*
 20130604
+20241207 - reformated using clang-format
 Jan Mojzis
 Public domain.
 */
@@ -20,13 +21,18 @@ char *porttostr(char *strbuf, const unsigned char *port) {
 
     if (!strbuf) strbuf = staticbuf; /* not thread-safe */
 
-    num = port[0]; num <<= 8; num |= port[1];
+    num = port[0];
+    num <<= 8;
+    num |= port[1];
     do {
-        num /= 10; ++len;
+        num /= 10;
+        ++len;
     } while (num);
     strbuf += len;
 
-    num = port[0]; num <<= 8; num |= port[1];
+    num = port[0];
+    num <<= 8;
+    num |= port[1];
     do {
         *--strbuf = '0' + (num % 10);
         num /= 10;
