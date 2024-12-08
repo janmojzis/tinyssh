178bcc12af3dd88f6bb5dc050bdc122fcad62d15
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-08 16:42:17 +0100

    subprocess_auth.c: clang-format

--- subprocess_auth.c
+++ subprocess_auth.c
@@ -1,5 +1,6 @@
 /*
 20140214
+20241208 - reformated using clang-format
 Jan Mojzis
 Public domain.
 */
@@ -42,18 +43,43 @@ static void check(uid_t uid, const char *d, const char *f, long long *err) {
     int e = 0;
 
     if (f) {
-        if (stat(f, &st) == -1) { log_w4("auth: unable to stat file: ", d, "/", f); e = 1; }
-        if (e == 0 && !S_ISREG(st.st_mode)) { errno = EACCES; log_w4("auth: non-regular file: ", d, "/", f); e = 1; }
+        if (stat(f, &st) == -1) {
+            log_w4("auth: unable to stat file: ", d, "/", f);
+            e = 1;
+        }
+        if (e == 0 && !S_ISREG(st.st_mode)) {
+            errno = EACCES;
+            log_w4("auth: non-regular file: ", d, "/", f);
+            e = 1;
+        }
     }
     else {
-        if (stat(d, &st) == -1) { log_w4("auth: unable to stat directory: ", d, "/", f); e = 1; }
-        if (e == 0 && !S_ISDIR(st.st_mode)) { errno = ENOTDIR; log_w4("auth: unable to stat directory: ", d, "/", f); e = 1; }
+        if (stat(d, &st) == -1) {
+            log_w4("auth: unable to stat directory: ", d, "/", f);
+            e = 1;
+        }
+        if (e == 0 && !S_ISDIR(st.st_mode)) {
+            errno = ENOTDIR;
+            log_w4("auth: unable to stat directory: ", d, "/", f);
+            e = 1;
+        }
     }
-    if (e == 0 && (st.st_mode & 022) != 0) { errno = EACCES; log_w4("auth: bad mode: directory writable by group or others: ", d, "/", f); e = 1; }
-    if (e == 0 && st.st_uid != uid && st.st_uid != 0) { errno = EACCES; log_w4("auth: bad owner: ", d, "/", f); e = 1; }
+    if (e == 0 && (st.st_mode & 022) != 0) {
+        errno = EACCES;
+        log_w4("auth: bad mode: directory writable by group or others: ", d,
+               "/", f);
+        e = 1;
+    }
+    if (e == 0 && st.st_uid != uid && st.st_uid != 0) {
+        errno = EACCES;
+        log_w4("auth: bad owner: ", d, "/", f);
+        e = 1;
+    }
 
-    if (e) *err = 1;
-    else log_d4("auth: path: ok: ", d, "/", f);
+    if (e)
+        *err = 1;
+    else
+        log_d4("auth: path: ok: ", d, "/", f);
 }
 
 int subprocess_auth_checkpath_(char *path, long long pathlen, uid_t uid) {
@@ -62,7 +88,8 @@ int subprocess_auth_checkpath_(char *path, long long pathlen, uid_t uid) {
 
     if (pathlen <= 16) return 0;
     if (!getcwd(path, pathlen)) return 0;
-    for (i = 0; i < pathlen; ++i) if (path[i] == 0) break;
+    for (i = 0; i < pathlen; ++i)
+        if (path[i] == 0) break;
     if (i == pathlen) return 0;
 
     check(uid, path, "authorized_keys", &err);
@@ -80,7 +107,9 @@ int subprocess_auth_checkpath_(char *path, long long pathlen, uid_t uid) {
     return (err == 0);
 }
 
-int subprocess_auth_authorizedkeys_(const char *keyname, const char *key, const char *dir, char *buf, long long bufmax) {
+int subprocess_auth_authorizedkeys_(const char *keyname, const char *key,
+                                    const char *dir, char *buf,
+                                    long long bufmax) {
 
     int fd = -1;
     int r;
@@ -94,7 +123,8 @@ int subprocess_auth_authorizedkeys_(const char *keyname, const char *key, const 
     do {
         r = getln(fd, buf, bufmax);
         if (r == -1) {
-            log_w3("auth: unable to read from file ", dir, "/.ssh/authorized_keys");
+            log_w3("auth: unable to read from file ", dir,
+                   "/.ssh/authorized_keys");
             return 0;
         }
         if (findnameandkey(keyname, key, buf)) return 1; /* authorized */
@@ -104,7 +134,7 @@ int subprocess_auth_authorizedkeys_(const char *keyname, const char *key, const 
 }
 
 /*
-The 'subprocess_auth' is used for authorization using 
+The 'subprocess_auth' is used for authorization using
 ~/.ssh/authorized_keys file.
 */
 int subprocess_auth(const char *account, const char *keyname, const char *key) {
@@ -116,7 +146,7 @@ int subprocess_auth(const char *account, const char *keyname, const char *key) {
     if (pid == -1) return -1;
     if (pid == 0) {
 
-        #define buf global_bspace2 /* reusing global buffer */
+#define buf global_bspace2 /* reusing global buffer */
         struct passwd *pw;
 
         if (!account || !keyname || !key) bug_inval();
@@ -124,7 +154,7 @@ int subprocess_auth(const char *account, const char *keyname, const char *key) {
 
         /* drop privileges */
         pw = getpwnam(account);
-        if (!pw) { 
+        if (!pw) {
             log_w3("auth: account ", account, ": not exist");
             global_die(111);
         }
@@ -144,8 +174,11 @@ int subprocess_auth(const char *account, const char *keyname, const char *key) {
         }
 
         /* authorization starts here */
-        if (!subprocess_auth_checkpath_((char *)buf, sizeof buf, pw->pw_uid)) global_die(111);
-        if (!subprocess_auth_authorizedkeys_(keyname, key, pw->pw_dir, (char *)buf, sizeof buf))  global_die(111);
+        if (!subprocess_auth_checkpath_((char *) buf, sizeof buf, pw->pw_uid))
+            global_die(111);
+        if (!subprocess_auth_authorizedkeys_(keyname, key, pw->pw_dir,
+                                             (char *) buf, sizeof buf))
+            global_die(111);
         /* authorization ends here */
 
         global_die(0);
