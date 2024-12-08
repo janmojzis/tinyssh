96cf38d980ae7efa3749f23e3336035f4d92745f
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-08 10:29:52 +0100

    savesync.[ch]: clang-format

--- savesync.c
+++ savesync.c
@@ -10,19 +10,17 @@ taken from nacl-20110221, from curvecp/savesync.c
 #include "savesync.h"
 #include "writeall.h"
 
-static int writesync(int fd,const void *x,long long xlen)
-{
-  if (writeall(fd,x,xlen) == -1) return -1;
-  return fsync(fd);
+static int writesync(int fd, const void *x, long long xlen) {
+    if (writeall(fd, x, xlen) == -1) return -1;
+    return fsync(fd);
 }
 
-int savesync(const char *fn,const void *x,long long xlen)
-{
-  int fd;
-  int r;
-  fd = open_write(fn);
-  if (fd == -1) return -1;
-  r = writesync(fd,x,xlen);
-  close(fd);
-  return r;
+int savesync(const char *fn, const void *x, long long xlen) {
+    int fd;
+    int r;
+    fd = open_write(fn);
+    if (fd == -1) return -1;
+    r = writesync(fd, x, xlen);
+    close(fd);
+    return r;
 }
--- savesync.h
+++ savesync.h
@@ -1,6 +1,6 @@
 #ifndef SAVESYNC_H____
 #define SAVESYNC_H____
 
-extern int savesync(const char *,const void *,long long);
+extern int savesync(const char *, const void *, long long);
 
 #endif
