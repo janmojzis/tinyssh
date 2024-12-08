b79d76b47cd676320d52e3d1f387ced79868fd00
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-08 09:52:53 +0100

    randommod.c: clang-format

--- randommod.c
+++ randommod.c
@@ -1,16 +1,19 @@
-/* taken from nacl-20110221, from curvecp/randommod.c */
+/*
+taken from nacl-20110221, from curvecp/randommod.c
+- reformated using clang-format
+*/
 #include "randombytes.h"
 #include "randommod.h"
 
 /* XXX: current implementation is limited to n<2^55 */
 
-long long randommod(long long n)
-{
-  long long result = 0;
-  long long j;
-  unsigned char r[32];
-  if (n <= 1) return 0;
-  randombytes(r,32);
-  for (j = 0;j < 32;++j) result = (result * 256 + (unsigned long long) r[j]) % n;
-  return result;
+long long randommod(long long n) {
+    long long result = 0;
+    long long j;
+    unsigned char r[32];
+    if (n <= 1) return 0;
+    randombytes(r, 32);
+    for (j = 0; j < 32; ++j)
+        result = (result * 256 + (unsigned long long) r[j]) % n;
+    return result;
 }
