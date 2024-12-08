436134170983484676a27e1112d1447a094f02d5
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-08 15:52:00 +0100

    subprocess_sign.c: clang-format

--- subprocess_sign.c
+++ subprocess_sign.c
@@ -1,5 +1,6 @@
 /*
 20140117
+20241208 - reformated using clang-format
 Jan Mojzis
 Public domain.
 */
@@ -22,13 +23,13 @@ Public domain.
 #include "subprocess.h"
 
 /*
-The 'subprocess_sign' function reads secret-key from 'keydir' and
-sings the data 'x' of length 'xlen' and returns it in 'y'.
-Caller is expecting 'y' of length 'ylen'. Signing is done 
-in a different process, so secret-key is in a separate
+The 'subprocess_sign' function reads secret-key from 'keydir' and signs the data
+'x' of length 'xlen' and returns it in 'y'. Caller is expecting 'y' of length
+'ylen'. Signing is done in a different process, so secret-key is in a separate
 memory space than rest of the program.
 */
-int subprocess_sign(unsigned char *y, long long ylen, const char *keydir, unsigned char *x, long long xlen) {
+int subprocess_sign(unsigned char *y, long long ylen, const char *keydir,
+                    unsigned char *x, long long xlen) {
 
     pid_t pid;
     int status, fromchild[2] = {-1, -1};
@@ -39,7 +40,11 @@ int subprocess_sign(unsigned char *y, long long ylen, const char *keydir, unsign
 
     if (open_pipe(fromchild) == -1) return -1;
     pid = fork();
-    if (pid == -1) { close(fromchild[0]); close(fromchild[1]); return -1; }
+    if (pid == -1) {
+        close(fromchild[0]);
+        close(fromchild[1]);
+        return -1;
+    }
     if (pid == 0) {
         unsigned char sk[sshcrypto_sign_SECRETKEYMAX];
         unsigned char sm[sshcrypto_sign_MAX + sshcrypto_hash_MAX];
@@ -52,13 +57,16 @@ int subprocess_sign(unsigned char *y, long long ylen, const char *keydir, unsign
             log_w2("sign: unable to change directory to ", keydir);
             global_die(111);
         }
-        if (load(sshcrypto_sign_secretkeyfilename, sk, sshcrypto_sign_secretkeybytes) == -1) {
-            log_w4("sign: unable to load secret-key from file ", keydir, "/", sshcrypto_sign_secretkeyfilename);
+        if (load(sshcrypto_sign_secretkeyfilename, sk,
+                 sshcrypto_sign_secretkeybytes) == -1) {
+            log_w4("sign: unable to load secret-key from file ", keydir, "/",
+                   sshcrypto_sign_secretkeyfilename);
             purge(sk, sizeof sk);
             global_die(111);
         }
-        if (sshcrypto_sign(sm, &smlen, x, sshcrypto_hash_bytes, sk) != 0) { 
-            log_w4("sign: unable to sign using secret-key from file ", keydir, "/", sshcrypto_sign_secretkeyfilename);
+        if (sshcrypto_sign(sm, &smlen, x, sshcrypto_hash_bytes, sk) != 0) {
+            log_w4("sign: unable to sign using secret-key from file ", keydir,
+                   "/", sshcrypto_sign_secretkeyfilename);
             purge(sk, sizeof sk);
             global_die(111);
         }
@@ -75,7 +83,10 @@ int subprocess_sign(unsigned char *y, long long ylen, const char *keydir, unsign
     }
     close(fromchild[1]);
     blocking_enable(fromchild[0]);
-    if (readall(fromchild[0], y, ylen) == -1) { close(fromchild[0]); return -1; }
+    if (readall(fromchild[0], y, ylen) == -1) {
+        close(fromchild[0]);
+        return -1;
+    }
     close(fromchild[0]);
 
     while (waitpid(pid, &status, 0) != pid) {}
