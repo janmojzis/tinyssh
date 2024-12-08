d956d977c9124270359a0af1bfdea8e78484fe67
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-08 09:24:32 +0100

    die.[ch]: clang-format

--- die.c
+++ die.c
@@ -8,14 +8,15 @@ void die_usage(const char *x) {
     global_die(100);
 }
 
-void die_fatal_(const char *fn, unsigned long long line, const char *trouble, const char *d, const char *f) {
+void die_fatal_(const char *fn, unsigned long long line, const char *trouble,
+                const char *d, const char *f) {
 
     if (d) {
-        if (f) log_9_(1, 0, fn, line, trouble, " ", d, "/", f, 0, 0, 0, 0);
-        else log_9_(1, 0, fn, line, trouble, " ", d, 0, 0, 0, 0, 0, 0);
+        if (f)
+            log_9_(1, 0, fn, line, trouble, " ", d, "/", f, 0, 0, 0, 0);
+        else
+            log_9_(1, 0, fn, line, trouble, " ", d, 0, 0, 0, 0, 0, 0);
     }
-    else {
-        log_9_(1, 0, fn, line, trouble, 0, 0, 0, 0, 0, 0, 0, 0);
-    }
+    else { log_9_(1, 0, fn, line, trouble, 0, 0, 0, 0, 0, 0, 0, 0); }
     global_die(111);
 }
--- die.h
+++ die.h
@@ -2,7 +2,8 @@
 #define DIE_H____
 
 extern void die_usage(const char *);
-extern void die_fatal_(const char *, unsigned long long, const char *, const char *, const char *);
+extern void die_fatal_(const char *, unsigned long long, const char *,
+                       const char *, const char *);
 #define die_fatal(a, b, cc) die_fatal_(__FILE__, __LINE__, (a), (b), (cc))
 
 #endif
