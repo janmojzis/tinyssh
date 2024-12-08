8f0b2e2464153f4e4cdaabe90cd0d008f076808f
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-08 16:46:45 +0100

    stringparser.[ch] clang-format

--- stringparser.c
+++ stringparser.c
@@ -1,5 +1,6 @@
 /*
 20140203
+20241208 - reformated using clang-format
 Jan Mojzis
 Public domain.
 */
@@ -13,11 +14,14 @@ Public domain.
 The 'stringparser' function parses items from
 comma-separated list. Empty strings are ignored.
 */
-long long stringparser(const unsigned char *buf, long long len, long long pos, const unsigned char **x, long long *xlen) {
+long long stringparser(const unsigned char *buf, long long len, long long pos,
+                       const unsigned char **x, long long *xlen) {
 
     long long i;
 
-    if (!buf || len < 0 || len > 1073741824 || pos < 0 || pos > 1073741824 || !x || !xlen) bug_inval();
+    if (!buf || len < 0 || len > 1073741824 || pos < 0 || pos > 1073741824 ||
+        !x || !xlen)
+        bug_inval();
 
     for (;;) {
         if (pos >= len) return 0;
--- stringparser.h
+++ stringparser.h
@@ -1,6 +1,7 @@
 #ifndef STRINGPARSER_H____
 #define STRINGPARSER_H____
 
-extern long long stringparser(const unsigned char *, long long, long long,  const unsigned char **, long long *);
+extern long long stringparser(const unsigned char *, long long, long long,
+                              const unsigned char **, long long *);
 
 #endif
