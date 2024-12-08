52e962a072e6f3af5e6e00a8b10ad94927f85453
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-08 16:50:22 +0100

    subprocess.h: clang-format

--- subprocess.h
+++ subprocess.h
@@ -3,10 +3,12 @@
 
 #include <sys/types.h>
 
-extern int subprocess_sign(unsigned char *, long long, const char *, unsigned char *, long long);
+extern int subprocess_sign(unsigned char *, long long, const char *,
+                           unsigned char *, long long);
 extern int subprocess_auth(const char *, const char *, const char *);
 
 extern int subprocess_auth_checkpath_(char *, long long, uid_t);
-extern int subprocess_auth_authorizedkeys_(const char *, const char *, const char *, char *, long long);
+extern int subprocess_auth_authorizedkeys_(const char *, const char *,
+                                           const char *, char *, long long);
 
 #endif
