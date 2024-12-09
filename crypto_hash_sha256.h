88a02853d28b5e4ec0566678ea70d54d0e8c7a96
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-09 13:55:41 +0100

    crypto_hash_sha256.h: clang-format

--- crypto_hash_sha256.h
+++ crypto_hash_sha256.h
@@ -2,7 +2,8 @@
 #define crypto_hash_sha256_H
 
 #define crypto_hash_sha256_tinyssh_BYTES 32
-extern int crypto_hash_sha256_tinyssh(unsigned char *,const unsigned char *,unsigned long long);
+extern int crypto_hash_sha256_tinyssh(unsigned char *, const unsigned char *,
+                                      unsigned long long);
 
 #define crypto_hash_sha256 crypto_hash_sha256_tinyssh
 #define crypto_hash_sha256_BYTES crypto_hash_sha256_tinyssh_BYTES
