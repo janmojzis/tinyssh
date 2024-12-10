0af0d5a2e4fc2634d8a9e1211b6fabb0f12f4c36
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-10 15:51:46 +0100

    _tinysshd-unauthenticated.c: clang-format

--- _tinysshd-unauthenticated.c
+++ _tinysshd-unauthenticated.c
@@ -14,46 +14,42 @@ Public domain.
 #include "packetparser.h"
 #include "global.h"
 
-static void cleanup(void) {
+static void cleanup(void) { global_purge(); }
 
-    global_purge();
-}
-
-__attribute__((noreturn))
-static void die_fatal(const char *trouble, const char *d, const char *fn) {
+__attribute__((noreturn)) static void die_fatal(const char *trouble,
+                                                const char *d, const char *fn) {
 
     cleanup();
 
     if (d) {
-        if (fn) log_f5(trouble, " ", d, "/", fn);
-        else log_f3(trouble, " ", d);
+        if (fn)
+            log_f5(trouble, " ", d, "/", fn);
+        else
+            log_f3(trouble, " ", d);
     }
-    else {
-        log_f1(trouble);
-    }
+    else { log_f1(trouble); }
     _exit(111);
 }
 
 static int packet_debug_(struct buf *b) {
     buf_purge(b);
-    buf_putnum8(b, SSH_MSG_DEBUG);              /* SSH_MSG_DEBUG  */
-    buf_putnum8(b, 1);                          /* always_display */
-    buf_putstring(b, "test message");           /* message in ISO-10646 UTF-8 encoding [RFC3629] */
-    buf_putstring(b, "");                       /* language tag [RFC3066] */
+    buf_putnum8(b, SSH_MSG_DEBUG); /* SSH_MSG_DEBUG  */
+    buf_putnum8(b, 1);             /* always_display */
+    buf_putstring(
+        b, "test message"); /* message in ISO-10646 UTF-8 encoding [RFC3629] */
+    buf_putstring(b, "");   /* language tag [RFC3066] */
     packet_put(b);
     return packet_sendall();
 }
 
-
 static unsigned char bspace[1024];
 static struct buf b;
-
 
 int main(int argc, char **argv) {
 
     pid_t pid;
-    int tochild[2] = { -1, -1 };
-    int fromchild[2] = { -1, -1 };
+    int tochild[2] = {-1, -1};
+    int fromchild[2] = {-1, -1};
     long long i;
 
     if (argc < 2) _exit(111);
@@ -80,8 +76,10 @@ int main(int argc, char **argv) {
     close(tochild[0]);
     close(fromchild[1]);
 
-    close(0); if (dup2(fromchild[0], 0) == -1) _exit(111);
-    close(1); if (dup2(tochild[1], 1) == -1) _exit(111);
+    close(0);
+    if (dup2(fromchild[0], 0) == -1) _exit(111);
+    close(1);
+    if (dup2(tochild[1], 1) == -1) _exit(111);
 
     signal(SIGPIPE, SIG_IGN);
 
@@ -89,12 +87,16 @@ int main(int argc, char **argv) {
 
     log_init(1, "_tinysshd-unauthenticated", 0, 0);
 
-    if (!packet_hello_receive()) die_fatal("unable to receive hello-string", 0, 0);
+    if (!packet_hello_receive())
+        die_fatal("unable to receive hello-string", 0, 0);
     if (!packet_hello_send()) die_fatal("unable to send hello-string", 0, 0);
 
     for (i = 1; i < 100 * PACKET_UNAUTHENTICATED_MESSAGES; ++i) {
         if (!packet_debug_(&b)) {
-            if (i < PACKET_UNAUTHENTICATED_MESSAGES) die_fatal("unable to send debug message - parent died too early", 0, 0);
+            if (i < PACKET_UNAUTHENTICATED_MESSAGES)
+                die_fatal(
+                    "unable to send debug message - parent died too early", 0,
+                    0);
             _exit(0); /* limitation works */
         }
 
