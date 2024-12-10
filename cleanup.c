a0a1a210a82596419ee1cbb9b5f39f0b00d70615
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-10 11:02:35 +0100

    cleanup.c: clang-format

--- cleanup.c
+++ cleanup.c
@@ -1,8 +1,11 @@
 #include "cleanup.h"
 
 void cleanup_(void *yv, long long ylen) {
-    volatile char *y = (volatile char *)yv; 
-    while (ylen > 0) { *y++ = 0; --ylen; }
+    volatile char *y = (volatile char *) yv;
+    while (ylen > 0) {
+        *y++ = 0;
+        --ylen;
+    }
 #ifdef __GNUC__
     __asm__ __volatile__("" : : "r"(yv) : "memory");
 #endif
