6d90c379a483d775bd6daa5834c171ba5f5cc533
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-11 13:45:39 +0100

    log.[ch]: clang format

--- log.c
+++ log.c
@@ -1,5 +1,6 @@
 /*
 20140202
+20241211 - reformated using clang-format
 Jan Mojzis
 Public domain.
 
@@ -25,15 +26,16 @@ int loglevel = 1;
 static int logflagfnln = 1;
 static int logflagsyslog = 0;
 
-static char chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRTSUVWXYZ0123456789";
+static char chars[] =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRTSUVWXYZ0123456789";
 
 void log_init(int level, const char *text, int line, int flagsyslog) {
 
     unsigned long long i;
 
     loglevel = level;
-    logtext  = text;
-    logflagfnln  = line;
+    logtext = text;
+    logflagfnln = line;
 
     for (i = 0; i < sizeof logstring; ++i) {
         logstring[i] = chars[randommod(sizeof chars - 1)];
@@ -48,10 +50,7 @@ void log_init(int level, const char *text, int line, int flagsyslog) {
     errno = 0;
 }
 
-
-char *log_string(void) {
-    return logstring;
-}
+char *log_string(void) { return logstring; }
 
 static char buf[257];
 static unsigned long long buflen = 0;
@@ -62,9 +61,7 @@ static void flush(void) {
         buf[buflen] = 0;
         syslog(LOG_INFO, "%s", buf);
     }
-    else {
-        writeall(2, buf, buflen);
-    }
+    else { writeall(2, buf, buflen); }
     buflen = 0;
     purge(buf, buflen);
 }
@@ -73,12 +70,16 @@ static void outs(const char *x) {
 
     long long i;
 
-    for(i = 0; x[i]; ++i) {
+    for (i = 0; x[i]; ++i) {
         if (buflen >= sizeof buf - 1) flush();
-        if (x[i] == '\n')  buf[buflen++] = '\n';
-        else if (x[i] < 32)  buf[buflen++] = '?';
-        else if (x[i] > 126) buf[buflen++] = '?';
-        else buf[buflen++] = x[i];
+        if (x[i] == '\n')
+            buf[buflen++] = '\n';
+        else if (x[i] < 32)
+            buf[buflen++] = '?';
+        else if (x[i] > 126)
+            buf[buflen++] = '?';
+        else
+            buf[buflen++] = x[i];
     }
 }
 
@@ -87,30 +88,24 @@ static void outnum(unsigned long long n) {
     outs(numtostr(b, n));
 }
 
-void log_9_(
-    int level
-    ,int ignoreerrno
-    ,const char *f
-    ,unsigned long long l
-    ,const char *s0
-    ,const char *s1
-    ,const char *s2
-    ,const char *s3
-    ,const char *s4
-    ,const char *s5
-    ,const char *s6
-    ,const char *s7
-    ,const char *s8
-)
-{
+void log_9_(int level, int ignoreerrno, const char *f, unsigned long long l,
+            const char *s0, const char *s1, const char *s2, const char *s3,
+            const char *s4, const char *s5, const char *s6, const char *s7,
+            const char *s8) {
     const char *s[9];
     long long i;
     const char *m;
 
     if (level > loglevel) return;
 
-    s[0] = s0; s[1] = s1; s[2] = s2; s[3] = s3;
-    s[4] = s4; s[5] = s5; s[6] = s6; s[7] = s7;
+    s[0] = s0;
+    s[1] = s1;
+    s[2] = s2;
+    s[3] = s3;
+    s[4] = s4;
+    s[5] = s5;
+    s[6] = s6;
+    s[7] = s7;
     s[8] = s8;
 
     switch (level) {
@@ -120,17 +115,19 @@ void log_9_(
         case 0:
             m = "usage";
             break;
-        case  1:
+        case 1:
             m = "fatal";
             break;
-        case  2: 
-            if (!ignoreerrno) m = "warning";
-            else m = "info";
+        case 2:
+            if (!ignoreerrno)
+                m = "warning";
+            else
+                m = "info";
             break;
-        case  3:
+        case 3:
             m = "debug";
             break;
-        case  4:
+        case 4:
             m = "tracing";
             break;
         default:
@@ -138,27 +135,29 @@ void log_9_(
             break;
     }
 
-
     /* name: session: level: message (error){file:line} */
 
     /* 'name:' */
     do {
-        if (level == 0)   break; /* don't print in usage level */
+        if (level == 0) break;    /* don't print in usage level */
         if (logflagsyslog) break; /* don't print in syslog mode */
-        outs(logtext); outs(": ");
+        outs(logtext);
+        outs(": ");
     } while (0);
 
     /* 'session:' */
     do {
-        if (level == 0) break;  /* don't print in usage level */
+        if (level == 0) break;   /* don't print in usage level */
         if (!logflagfnln) break; /* don't print when disabled   */
-        outs(logstring); outs(": ");
+        outs(logstring);
+        outs(": ");
     } while (0);
 
     /* 'level:' */
     do {
         if (level == 0) break; /* don't print in usage level */
-        outs(m); outs(": ");
+        outs(m);
+        outs(": ");
     } while (0);
 
     /* 'message' */
@@ -167,18 +166,25 @@ void log_9_(
 
     /* '(error)' */
     do {
-        if (!errno)      break; /* don't print when errno = 0 */
+        if (!errno) break;      /* don't print when errno = 0 */
         if (ignoreerrno) break; /* don't print when disabled   */
-        outs("("); outs(e_str(errno)); outs(")");
+        outs("(");
+        outs(e_str(errno));
+        outs(")");
     } while (0);
 
     /* {file:line} */
     do {
-        if (level == 0) break;  /* don't print in usage level */
-        if (!f)          break;  /* don't print when no f      */
-        if (!l)          break;  /* don't print when no l      */
-        if (!logflagfnln && level != -1) break; /* don't print when disabled   */
-        outs("{"); outs(f); outs(":"); outnum(l); outs("}");
+        if (level == 0) break; /* don't print in usage level */
+        if (!f) break;         /* don't print when no f      */
+        if (!l) break;         /* don't print when no l      */
+        if (!logflagfnln && level != -1)
+            break; /* don't print when disabled   */
+        outs("{");
+        outs(f);
+        outs(":");
+        outnum(l);
+        outs("}");
     } while (0);
 
     outs("\n");
