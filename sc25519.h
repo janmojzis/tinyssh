486287f3eea78e72c024127a15e9b4d9e0886632
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-11 13:33:36 +0100

    sc25519.[ch]: clang-format

--- sc25519.h
+++ sc25519.h
@@ -2,6 +2,7 @@
 #define SC25519_H____
 
 extern void sc25519_reduce(unsigned char *);
-extern void sc25519_muladd(unsigned char *, const unsigned char *, const unsigned char *, const unsigned char *);
+extern void sc25519_muladd(unsigned char *, const unsigned char *,
+                           const unsigned char *, const unsigned char *);
 
 #endif
