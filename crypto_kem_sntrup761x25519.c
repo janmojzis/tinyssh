71014e450702c967cac81695415c65f451d3cd98
Jan Mojžíš <jan.mojzis@gmail.com>
2024-12-17 07:39:57 +0100

    crypto_kem_sntrup761x25519.c: remove *BYTES macros

--- crypto_kem_sntrup761x25519.c
+++ crypto_kem_sntrup761x25519.c
@@ -12,23 +12,11 @@ Public domain.
 #include "crypto_kem_sntrup761.h"
 #include "crypto_kem_sntrup761x25519.h"
 
-#define sntrup_BYTES crypto_kem_sntrup761_BYTES
-#define x25519_BYTES crypto_dh_x25519_BYTES
-#define sx_BYTES sntrup_BYTES + x25519_BYTES
-
-#define sntrup_SECRETKEYBYTES crypto_kem_sntrup761_SECRETKEYBYTES
-#define x25519_SECRETKEYBYTES crypto_dh_x25519_SECRETKEYBYTES
-#define sx_SECRETKEYBYTES sntrup_SECRETKEYBYTES + x25519_SECRETKEYBYTES
-
-#define sntrup_PUBLICKEYBYTES crypto_kem_sntrup761_PUBLICKEYBYTES
-#define x25519_PUBLICKEYBYTES crypto_dh_x25519_PUBLICKEYBYTES
-#define sx_PUBLICKEYBYTES sntrup_PUBLICKEYBYTES + x25519_PUBLICKEYBYTES
-
 int crypto_kem_sntrup761x25519_tinyssh_enc(unsigned char *c, unsigned char *k,
                                            const unsigned char *pk) {
 
-    unsigned char onetimesk[x25519_SECRETKEYBYTES];
-    unsigned char buf[sx_BYTES];
+    unsigned char onetimesk[crypto_dh_x25519_SECRETKEYBYTES];
+    unsigned char buf[crypto_kem_sntrup761_BYTES + crypto_dh_x25519_BYTES];
 
     /* sntrup761 */
     crypto_kem_sntrup761_enc(c, buf, pk);
@@ -37,7 +25,7 @@ int crypto_kem_sntrup761x25519_tinyssh_enc(unsigned char *c, unsigned char *k,
 
     /* x25519 */
     crypto_dh_x25519_keypair(/*onetimepk*/ c, onetimesk);
-    crypto_dh_x25519(buf + sntrup_BYTES, pk, onetimesk);
+    crypto_dh_x25519(buf + crypto_kem_sntrup761_BYTES, pk, onetimesk);
 
     /* hash together sntrup459176 KEM-key and x25519 shared secret */
     crypto_hash_sha512(k, buf, sizeof buf);
@@ -53,7 +41,7 @@ int crypto_kem_sntrup761x25519_tinyssh_dec(unsigned char *k,
                                            const unsigned char *c,
                                            const unsigned char *sk) {
 
-    unsigned char buf[sx_BYTES];
+    unsigned char buf[crypto_kem_sntrup761_BYTES + crypto_dh_x25519_BYTES];
 
     /* sntrup761 */
     crypto_kem_sntrup761_dec(buf, c, sk);
@@ -61,7 +49,7 @@ int crypto_kem_sntrup761x25519_tinyssh_dec(unsigned char *k,
     c += crypto_kem_sntrup761_CIPHERTEXTBYTES;
 
     /* x25519 */
-    crypto_dh_x25519(buf + sntrup_BYTES, c, sk);
+    crypto_dh_x25519(buf + crypto_kem_sntrup761_BYTES, c, sk);
 
     /* hash together sntrup459176 KEM-key and x25519 shared secret */
     crypto_hash_sha512(k, buf, sizeof buf);
@@ -79,8 +67,8 @@ int crypto_kem_sntrup761x25519_tinyssh_keypair(unsigned char *pk,
     crypto_kem_sntrup761_keypair(pk, sk);
 
     /* x25519 */
-    crypto_dh_x25519_keypair(pk + sntrup_PUBLICKEYBYTES,
-                             sk + sntrup_SECRETKEYBYTES);
+    crypto_dh_x25519_keypair(pk + crypto_kem_sntrup761_PUBLICKEYBYTES,
+                             sk + crypto_kem_sntrup761_SECRETKEYBYTES);
 
     return 0;
 }
